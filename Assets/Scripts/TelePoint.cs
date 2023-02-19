f1834e5ae9a58f4a62979487d6315c76549b9c3a
SaherPersonal <saher.ziauddin@dal.ca>
2023-02-19 00:11:42 -0400

    - implemented delay before teleporting

--- Assets/Scripts/TelePoint.cs
+++ Assets/Scripts/TelePoint.cs
@@ -8,8 +8,10 @@ public class TelePoint : MonoBehaviour
     public bool isEnabled = false;
     // target
     public Transform target;
+    public float delay = 1f;
 
     public UnityEvent OnTeleportation;
+
 
     // on trigger2d enter
     private void OnTriggerEnter2D(Collider2D collision)
@@ -17,10 +19,17 @@ public class TelePoint : MonoBehaviour
         // if collision is player
         if (collision.gameObject.CompareTag("Player"))
         {
-            // set player position to target position
-            collision.gameObject.transform.position = target.position;
-            OnTeleportation?.Invoke();
+            StartCoroutine(TeleportPlayer(collision));
         }
     }
 
+    IEnumerator TeleportPlayer(Collider2D go)
+    {
+        yield return new WaitForSeconds(delay);
+        Debug.Log("Teleporting player..");
+
+        // set player position to target position
+        go.gameObject.transform.position = target.position;
+        OnTeleportation?.Invoke();
+    }
 }
